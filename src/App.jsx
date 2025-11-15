ee1b4a4014f4fee9922d0668d9ccf727dc712cf5
VittorioDauria94 <dauriavittorio94@gmail.com>
2025-11-15 22:13:19 +0100

    feat: added the actors list

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Card from "./components/Card";
 
 function App() {
   const [actresses, setActresses] = useState([]);
+  const [actors, setActors] = useState([]);
 
   useEffect(() => {
     getActresses();
+    getActors();
   }, []);
 
   function getActresses() {
@@ -15,11 +17,42 @@ function App() {
     });
   }
 
+  function getActors() {
+    axios.get("https://lanciweb.github.io/demo/api/actors/").then((e) => {
+      setActors(e.data)
+    })
+  }
+
   return (
     <>
       <div className="container mt-5 mb-5">
+        <h2>Actresses</h2>
         <div className="row row-cols-2 row-cols-lg-5 g-4">
           {actresses.map(
+            ({
+              id,
+              name,
+              birth_year,
+              nationality,
+              awards,
+              biography,
+              image,
+            }) => (
+              <Card
+                key={id}
+                name={name}
+                birth={birth_year}
+                nationality={nationality}
+                awards={awards}
+                bio={biography}
+                src={image}
+              />
+            )
+          )}
+        </div>
+        <h2>Actors</h2>
+        <div className="row row-cols-2 row-cols-lg-5 g-4">
+          {actors.map(
             ({
               id,
               name,
